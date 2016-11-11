597f605ab4b678ed1c8744dcac67e8bd84589de2
Niels Holt <niels@steenbuchholt.dk>
2016-11-11 11:54:54 +0100

    Update to use grunt-fcoo-grunt-plugin^2.0.0 Using fcoo/modernizr-javascript

--- dist/leaflet-zoom-modernizr.js
+++ dist/leaflet-zoom-modernizr.js
@@ -1,53 +1,49 @@
 /****************************************************************************
-	leaflet-zoom-modernizr.js,
+    leaflet-zoom-modernizr.js,
 
-	(c) 2016, FCOO
+    (c) 2016, FCOO
 
-	https://github.com/FCOO/leaflet-zoom-modernizr
-	https://github.com/FCOO
+    https://github.com/FCOO/leaflet-zoom-modernizr
+    https://github.com/FCOO
 
 ****************************************************************************/
-;(function ($, L, window, document, undefined) {
-	"use strict";
+(function ($, L/*, window, document, undefined*/) {
+    "use strict";
 
-	L.Map.mergeOptions({
-		zoomModernizr: true
-	});
+    L.Map.mergeOptions({
+        zoomModernizr: true
+    });
 
-	L.Map.ZoomModernizr = L.Handler.extend({
-		addHooks: function () {
-			this._zoomModernizr();
-	    this._map.on('zoomend', this._zoomModernizr, this);
-		},
+    L.Map.ZoomModernizr = L.Handler.extend({
+        addHooks: function () {
+            this._zoomModernizr();
+            this._map.on('zoomend', this._zoomModernizr, this);
+        },
 
-	  removeHooks: function () {
-		  this._map.off('zoomend', this._zoomModernizr, this);
-	  },
+      removeHooks: function () {
+          this._map.off('zoomend', this._zoomModernizr, this);
+      },
 
-	  _zoomModernizr: function ( /*e*/ ) {
-			var map					= this._map,
-					zoom				= map.getZoom() || 0,
-					minZoom			= map.getMinZoom() || 0,
-					maxZoom			= map.getMaxZoom(),
-					$container	= $(map.getContainer()),
-					i;
+      _zoomModernizr: function ( /*e*/ ) {
+            var map        = this._map,
+                zoom       = map.getZoom() || 0,
+                minZoom    = map.getMinZoom() || 0,
+                maxZoom    = map.getMaxZoom(),
+                $container = $(map.getContainer()),
+                i;
 
-			if (maxZoom === Infinity)
-				maxZoom = L.TileLayer.prototype.options.maxZoom;
+            if (maxZoom === Infinity)
+                maxZoom = L.TileLayer.prototype.options.maxZoom;
 
-			for (i=minZoom; i<=maxZoom; i++ ){
-				$container.toggleClass( 'leaflet-zoom-' + i						, i == zoom	);
-				$container.toggleClass( 'leaflet-zoom-' + i + '-up'		,	i <= zoom	);
-				$container.toggleClass( 'leaflet-zoom-' + i + '-down'	,	i >= zoom	);
+            for (i=minZoom; i<=maxZoom; i++ )
+                $container
+                    .modernizrToggle( 'leaflet-zoom-' + i          , i == zoom )
+                    .modernizrToggle( 'leaflet-zoom-' + i + '-up'  , i <= zoom )
+                    .modernizrToggle( 'leaflet-zoom-' + i + '-down', i >= zoom );
+      }
+    });
 
-				$container.toggleClass( 'no-leaflet-zoom-' + i						, i != zoom	);
-				$container.toggleClass( 'no-leaflet-zoom-' + i + '-up'		,	i > zoom	);
-				$container.toggleClass( 'no-leaflet-zoom-' + i + '-down'	,	i < zoom	);
-			}
-	  }
-	});
-
-	L.Map.addInitHook('addHandler', 'zoomModernizr', L.Map.ZoomModernizr);
+    L.Map.addInitHook('addHandler', 'zoomModernizr', L.Map.ZoomModernizr);
 
 
 }(jQuery, L, this, document));
