37d4663f2c5659dad5d8e2bc51b682405eb28c52
Niels Holt <niels@steenbuchholt.dk>
2022-02-02 15:38:07 +0100

    Update dependencies

--- demo/bower_components.js
+++ demo/bower_components.js
@@ -37,9 +37,13 @@
     is_ios = (os == "ios");
 
     /**************************************************
-    Global Switches for Leaflet
-    From Leaflet 1.2.0:
-    Switch	        Description
+    From Leaflet 1.7.1:
+    Global Switches
+    Global switches are created for rare cases and generally make Leaflet to not detect a particular browser feature even if it's there.
+    You need to set the switch as a global variable to true before including Leaflet on the page, like this:
+    <script>L_NO_TOUCH = true;</script>
+    <script src="leaflet.js"></script>
+    Switch	Description
     L_NO_TOUCH	    Forces Leaflet to not use touch events even if it detects them.
     L_DISABLE_3D	Forces Leaflet to not use hardware-accelerated CSS 3D transforms for positioning (which may cause glitches in some rare environments) even if they're supported.
     **************************************************/
@@ -25056,6 +25060,26 @@ return jQuery;
             this._rawPxBounds.max.add(p)
         ]);
     };
+
+}(L, this, document));
+
+
+
+
+;
+/****************************************************************************
+	leaflet-bower-bugfix-switches.js,
+
+	(c) 2022, FCOO
+
+	https://github.com/FCOO/leaflet-bower-bugfix-switches
+	https://github.com/FCOO
+
+****************************************************************************/
+(function (/*L, window, document, undefined*/) {
+	"use strict";
+
+
 
 }(L, this, document));
 
